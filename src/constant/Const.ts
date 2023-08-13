e1fb59ce2d5086314c12c69bcf01375f33c32e4e
plla2 <jhbljs92@naver.com>
2023-08-13 22:48:09 +0900

    Const navLinks 생성

---
+++ src/constant/Const.ts
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "skills",
+    title: "Skills",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
