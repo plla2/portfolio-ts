79a8750732aba85bb02f8480cabe9c42458e30a1
plla2 <jhbljs92@naver.com>
2023-08-13 22:21:12 +0900

    💄 공통 styles 파일

---
+++ src/styles.ts
@@ -0,0 +1,10 @@
+const styles = {
+  paddingX: "sm:px-16 px-6",
+  paddingY: "sm:py-16 py-6",
+  padding: "sm:px-16 px-6 sm:py-16 py-10",
+
+  heroHeadText:
+    "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+};
+
+export { styles };
