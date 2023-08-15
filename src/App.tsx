ede3399d2f7048d62bd6ec9668fb0ee0277590c5
plla2 <jhbljs92@naver.com>
2023-08-15 14:39:26 +0900

    🚀 deploy build

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/nav/Navbar";
+import Hero from "./pages/Hero";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
