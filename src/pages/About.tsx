b9e42265d3b5c7f85609922a937471819e854589
plla2 <jhbljs92@naver.com>
2023-08-16 02:30:42 +0900

    📝 About section

---
+++ src/pages/About.tsx
@@ -0,0 +1,46 @@
+import SectionTitle from "../components/Section/SectionTitle";
+import { LazyMotion, domAnimation, m } from "framer-motion";
+import { introduction } from "../constant/Const";
+
+const About = () => {
+  return (
+    <div id="about" className="w-full flex justify-center overflow-hidden-web">
+      <div className="w-full xl:w-[80%] flex flex-col pb-16">
+        <div className="w-full">
+          <SectionTitle title="ABOUT ME" subTitle="INTRODUCTION" />
+        </div>
+        <div className="w-full flex flex-col-reverse sm:flex-row">
+          <div className="w-full md:w-[50%] md:h-full flex items-center mt-10">
+            <LazyMotion features={domAnimation} strict>
+              <m.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{
+                  duration: 1,
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 20,
+                }}
+                style={{ fontFamily: "GmarketSansTTFMedium" }}
+                className="text-grayscale-0 p-6 flex flex-col gap-6"
+              >
+                <span>{introduction.text[0]}</span>
+                <span>{introduction.text[1]}</span>
+                <span>{introduction.text[2]}</span>
+                <span>{introduction.text[3]}</span>
+                <span>{introduction.text[4]}</span>
+              </m.p>
+            </LazyMotion>
+          </div>
+          <div className="w-full md:w-[50%] flex h-full items-end justify-start">
+            <div className="w-[80%] lg:w-[50%] h-[300px] sm:h-[350px] flex justify-center">
+              {/* <ImageSlide images={profileImg.image} /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
