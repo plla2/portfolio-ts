efccf70a7024a4eeac16176f810161bd8fce7580
plla2 <jhbljs92@naver.com>
2023-08-17 01:04:52 +0900

    🐛 project section title겹침 수정

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -8,10 +8,10 @@ const Project = () => {
       className="w-full overflow-hidden-web flex justify-center"
     >
       <div className="w-full min-h-[800px] h-screen xl:w-[80%] relative flex flex-col items-center justify-center pb-44">
-        <div className="w-full h-[180%] mt-[10%] mb-[-10%]">
+        <div className="w-full h-[180%] mt-[10%]">
           <SectionTitle title="PROJECTS" subTitle="What I have done so far" />
         </div>
-        <div className="w-full h-[80%] z-10 flex justify-center">
+        <div className="w-full h-[80%] z-10 flex justify-center mt-20">
           <div className="w-full xl:w-[90%] h-[-10%]">
             <ProjectCards />
           </div>
