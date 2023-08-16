75bf8eddacfcc5669234760951a5e8a5a33945ba
plla2 <jhbljs92@naver.com>
2023-08-17 00:07:03 +0900

    🍱 icons,webp파일 import

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,23 @@
 import avatar from "./Avatar/avatar.jpeg";
+import phoneIcon from "./Icons/phone-fill.svg";
+import githubIcon from "./Icons/github.svg";
+import mailIcon from "./Icons/mail.svg";
+import blogIcon from "./Icons/tistory.svg";
+import moviepop from "./ProjectImg/moviePoP.webp";
+import lofi from "./ProjectImg/lofi.webp";
+import portfolio from "./ProjectImg/portfolio.webp";
+import linkIcon from "./Icons/link-bold.svg";
+import reviewIcon from "./Icons/blog.svg";
 
-export { avatar };
+export {
+  avatar,
+  phoneIcon,
+  githubIcon,
+  mailIcon,
+  blogIcon,
+  moviepop,
+  lofi,
+  portfolio,
+  linkIcon,
+  reviewIcon,
+};
