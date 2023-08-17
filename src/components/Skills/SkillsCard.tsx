047fa9c63b000f9b7a52659577fd6bb09ec3d0d1
plla2 <jhbljs92@naver.com>
2023-08-18 01:20:09 +0900

    📝 skills mapping, motion

---
+++ src/components/Skills/SkillsCard.tsx
@@ -0,0 +1,45 @@
+import { LazyMotion, domAnimation, m } from "framer-motion";
+import { skills } from "../../constant/Const";
+
+const SkillsCard = () => {
+  return (
+    <div className="flex">
+      <LazyMotion features={domAnimation} strict>
+        {skills.map((skill, index) => (
+          <m.div
+            initial={{ scale: 0.8 }}
+            animate={{ rotate: [0, 10, 0] }}
+            transition={{
+              duration: 1,
+              repeat: Infinity,
+              repeatType: "loop",
+              ease: "linear",
+            }}
+            style={{ zIndex: `${index + 1}`, transition: "all 0.6s" }}
+            key={index}
+            className="card w-[220px] h-[240px] flex flex-col items-center bg-[#FFF6E0] rounded-xl border-4 border-[#FFBFBF] cursor-pointer"
+          >
+            <div className="w-full h-[60px] flex items-center gap-2 p-1 flex-col">
+              <img
+                src={skill.icon}
+                alt="스킬 아이콘"
+                className="h-[40px] flex justify-center items-center w-[50px] rounded-[50%] p-1 object-contain"
+              />
+              <span
+                className="text-lg"
+                style={{ fontFamily: "GmarketSansTTFMedium" }}
+              >
+                {skill.title}
+              </span>
+              <span className="text-center text-sm py-3 px-4">
+                {skill.desc}
+              </span>
+            </div>
+          </m.div>
+        ))}
+      </LazyMotion>
+    </div>
+  );
+};
+
+export default SkillsCard;
