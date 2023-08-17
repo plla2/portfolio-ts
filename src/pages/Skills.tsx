0bb64f34401918571cd2a0a9295a206f02d5d3f7
plla2 <jhbljs92@naver.com>
2023-08-18 01:20:50 +0900

    📝 skills 정리 컴포넌트

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,25 +1,39 @@
 import SectionTitle from "../components/Section/SectionTitle";
+import MarqueeCards from "../components/Skills/MarqueeCards";
+import SkillsCard from "../components/Skills/SkillsCard";
+import SkillsLibrary from "../components/Skills/SkillsLibrary";
 
 const Skills = () => {
   return (
-    <div
-      id="skills"
-      className="w-full overflow-hidden-web flex justify-center mt-16"
-    >
-      <div className="w-full min-h-[800px] flex flex-col xl:w-[80%]">
-        <div className="w-full">
-          <SectionTitle title="SKILLS" subTitle="WHAT I DO" />
-        </div>
-        <div className="xl:border-l-2 xl:border-r-2 xl:border-[#FFF5E4] h-full">
-          <div className="relative">
-            <div className="absolute top-[45px] w-full h-[1px] bg-[#FFF5E4]"></div>
+    <>
+      <div
+        id="skills"
+        className="w-full overflow-hidden-web flex flex-col items-center mt-16"
+      >
+        <div className="w-full min-h-[800px] flex flex-col xl:w-[80%] items-center">
+          <div className="w-full my-10">
+            <SectionTitle title="SKILLS" subTitle="WHAT I DO" />
           </div>
-          <div className="relative">
-            <div className="absolute top-[45px] w-full h-[1px] bg-[#FFF5E4]"></div>
+          <div className=" w-[85%] xl:border-l-2 xl:border-r-2 xl:border-[#FFF5E4] h-fit">
+            <div className="relative">
+              <div className="absolute top-[45px] w-full h-[1px] bg-[#FFF5E4]"></div>
+              <MarqueeCards direction="left">
+                <SkillsCard />
+              </MarqueeCards>
+            </div>
+            <div className="relative">
+              <div className="absolute top-[45px] w-full h-[1px] bg-[#FFF5E4]"></div>
+              <MarqueeCards direction="right">
+                <SkillsCard />
+              </MarqueeCards>
+            </div>
+          </div>
+          <div className="mt-16 w-[85%] xl:w-[80%]">
+            <SkillsLibrary />
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
