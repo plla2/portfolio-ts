c3cab0d5c51844fbca2a27985b77cdc7efb73ea8
plla2 <jhbljs92@naver.com>
2023-08-19 00:20:22 +0900

    🩹 Contact 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import Hero from "./pages/Hero";
 import About from "./pages/About";
 import Project from "./pages/Project";
 import Skills from "./pages/Skills";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <Hero />
       <About />
       <Project />
       <Skills />
-    </div>
+      <Contact />
+    </>
   );
 };
 
