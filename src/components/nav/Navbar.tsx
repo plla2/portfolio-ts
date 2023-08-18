019fa68872df29798bdb5c4a9719469424edcbf7
plla2 <jhbljs92@naver.com>
2023-08-19 00:27:23 +0900

    ⚡️ react-scroll 활용 navbar수정

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,24 +1,23 @@
 import { useState } from "react";
 import { styles } from "../../styles";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import close from "../../assets/navIcon/close.svg";
 import menu from "../../assets/navIcon/menu.svg";
 import { navLinks } from "../../constant/Const";
 import { LazyMotion, domAnimation, m } from "framer-motion";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-  const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary backdrop-blur-sm`}
+      className={`${styles.paddingX} w-full flex items-center py-2 fixed top-0 z-20 bg-primary backdrop-blur-sm`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to="/"
           className="flex items-center gap-2"
           onClick={() => {
-            setActive("");
             window.scrollTo(0, 0);
           }}
         >
@@ -32,21 +31,23 @@ const Navbar = () => {
             </m.p>
           </LazyMotion>
         </Link>
-        <ul className="list-none hidden sm:flex flex-row gap-10 text-grayscale-200">
+        <ul className="hidden sm:flex flex-row gap-10 text-grayscale-200">
           {navLinks.map((link) => {
             return (
               <li
                 key={link.id}
-                className={`${
-                  active === link.title
-                    ? "text-primary-400"
-                    : "text-grayscale-200"
-                } hover:text-primary-400 text-[18px] font-medium cursor-pointer`}
-                onClick={() => {
-                  setActive(link.title);
-                }}
+                className="text-[18px] font-bold cursor-pointer before:ml-auto after:content-[''] after:w-[0%] after:h-[2px] after:bg-primary-600 after:block after:duration-500 before:content-[''] before:w-[0%] before:h-[2px] before:bg-primary-600 before:block before:duration-500 hover:after:w-full hover:before:w-full"
               >
-                <a href={`#${link.id}`}>{link.title}</a>
+                <Link
+                  activeClass="active"
+                  to={link.id}
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={200}
+                >
+                  {link.title}
+                </Link>
               </li>
             );
           })}
@@ -62,22 +63,26 @@ const Navbar = () => {
               !toggle ? "hidden" : "flex"
             } pt-15 p-6 absolute top-2 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl bg-primary-400 bg-opacity-50`}
           >
-            <ul className="list-none flex justify-end items-start flex-col gap-4">
+            <ul className="flex justify-end items-start flex-col gap-4">
               {navLinks.map((link) => {
                 return (
                   <li
                     key={link.id}
-                    className={`${
-                      active === link.title
-                        ? "text-grayscale-950"
-                        : "text-grayscale-200"
-                    } font-medium cursor-pointer hover:bg-primary-600 hover:rounded-md`}
-                    onClick={() => {
-                      setActive(link.title);
-                      setToggle(!toggle);
-                    }}
+                    className="text-[18px] font-bold cursor-pointer"
                   >
-                    <a href={`#${link.title}`}>{link.title}</a>
+                    <Link
+                      activeClass="active"
+                      to={link.id}
+                      spy={true}
+                      smooth={true}
+                      offset={0}
+                      duration={200}
+                      onClick={() => {
+                        setToggle(!toggle);
+                      }}
+                    >
+                      {link.title}
+                    </Link>
                   </li>
                 );
               })}
