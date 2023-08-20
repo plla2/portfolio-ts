c2a2b53c230550879dc32d0553ced65d027121c9
plla2 <jhbljs92@naver.com>
2023-08-21 00:14:33 +0900

    🎨 Loader 조건부렌더링 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,25 @@ import About from "./pages/About";
 import Project from "./pages/Project";
 import Skills from "./pages/Skills";
 import Contact from "./pages/Contact";
+import { useState } from "react";
+import Loader from "./components/Loader/Loader";
 
 const App = () => {
+  const [showLoader, setShowLoader] = useState(true);
   return (
     <>
-      <Navbar />
-      <Hero />
-      <About />
-      <Project />
-      <Skills />
-      <Contact />
+      {showLoader ? (
+        <Loader setShowLoader={setShowLoader} />
+      ) : (
+        <>
+          <Navbar />
+          <Hero />
+          <About />
+          <Project />
+          <Skills />
+          <Contact />
+        </>
+      )}
     </>
   );
 };
