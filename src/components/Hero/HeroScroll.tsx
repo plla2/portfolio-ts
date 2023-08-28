230e8734f2735b02979f3ded9cfd0e2df2b92fb9
plla2 <jhbljs92@naver.com>
2023-08-28 16:26:25 +0900

    🩹 scroll 컴포넌트 a태그 삭제

--- src/components/Hero/HeroScroll.tsx
+++ src/components/Hero/HeroScroll.tsx
@@ -7,15 +7,13 @@ const HeroScroll = () => {
       className="absolute xs:bottom-10 bottom-12 flex flex-col justify-center items-center"
     >
       <p className="pb-2 text-[12px] text-primary-400">SCROLL DOWN</p>
-      <a href="#about">
-        <div className="w-[35px] h-[64px] rounded-3xl border-4 border-primary-400 flex justify-center items-start p-2">
-          <m.div
-            animate={{ y: [0, 24, 0] }}
-            transition={{ duration: 2, repeat: Infinity, repeatType: "loop" }}
-            className="w-3 h-3 rounded-full bg-primary-400"
-          />
-        </div>
-      </a>
+      <div className="w-[35px] h-[64px] rounded-3xl border-4 border-primary-400 flex justify-center items-start p-2">
+        <m.div
+          animate={{ y: [0, 24, 0] }}
+          transition={{ duration: 2, repeat: Infinity, repeatType: "loop" }}
+          className="w-3 h-3 rounded-full bg-primary-400"
+        />
+      </div>
     </div>
   );
 };
