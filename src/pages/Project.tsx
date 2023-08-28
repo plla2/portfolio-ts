d09d469aac64de8483e494de62c5d97fa9a87b3d
plla2 <jhbljs92@naver.com>
2023-08-28 16:28:44 +0900

    🎨 scroll 컴포넌트 조건 렌더링 수정

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,22 +1,42 @@
+import { useEffect, useState } from "react";
+import HeroScroll from "../components/Hero/HeroScroll";
 import ProjectCards from "../components/Projects/ProjectCards";
 import SectionTitle from "../components/Section/SectionTitle";
+import { LazyMotion, domAnimation } from "framer-motion";
 
 const Project = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setScreenWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div
       id="projects"
       className="w-full overflow-hidden-web flex justify-center"
     >
-      <div className="w-full min-h-[800px] h-screen xl:w-[80%] relative flex flex-col items-center justify-center pb-44">
-        <div className="w-full mb-9 mt-[7%]">
-          <SectionTitle title="PROJECTS" subTitle="What I have done so far" />
-        </div>
-        <div className="w-full h-[80%] z-10 flex justify-center mt-22">
-          <div className="w-full xl:w-[90%] h-[-10%] ">
-            <ProjectCards />
+      <LazyMotion features={domAnimation}>
+        <div className="w-full min-h-[800px] h-screen xl:w-[80%] relative flex flex-col items-center justify-center pb-36">
+          <div className="w-full mb-9 mt-[7%]">
+            <SectionTitle title="PROJECTS" subTitle="What I have done so far" />
+          </div>
+          <div className="w-full h-[80%] z-10 flex justify-center mt-22">
+            <div className="w-full lg:h-[120%] overflow-auto">
+              <div className="absolute top-[67%] left-[10%]">
+                {screenWidth <= 1023 && 560 <= screenWidth && <HeroScroll />}
+              </div>
+              <ProjectCards />
+            </div>
           </div>
         </div>
-      </div>
+      </LazyMotion>
     </div>
   );
 };
