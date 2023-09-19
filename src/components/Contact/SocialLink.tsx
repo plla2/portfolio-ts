ecfebfb4c93fdf370332a9d446e1e0292ffbda27
plla2 <jhbljs92@naver.com>
2023-09-20 02:05:28 +0900

    ♻️ social 컴포넌트 반복코드 리팩토링

--- src/components/Contact/SocialLink.tsx
+++ src/components/Contact/SocialLink.tsx
@@ -4,18 +4,17 @@ import { icons } from "../../constant/Const";
 const SocialLink = () => {
   return (
     <div className="w-full h-[100px] bg-[#474E68] flex justify-center items-center gap-10 p-6">
-      <a href="https://github.com/plla2" target="_blank">
-        <FontAwesomeIcon
-          icon={icons.faGithub}
-          className="text-3xl text-grayscale-0 hover:scale-110  transition-all duration-75"
-        />
-      </a>
-      <a href="https://jhbljs92.tistory.com/" target="_blank">
-        <FontAwesomeIcon
-          icon={icons.faSquarePen}
-          className="text-3xl text-grayscale-0 hover:scale-110  transition-all duration-75"
-        />
-      </a>
+      {[
+        { link: "https://github.com/plla2", icon: icons.faGithub },
+        { link: "https://jhbljs92.tistory.com/", icon: icons.faSquarePen },
+      ].map((social, index) => (
+        <a href={social.link} target="_blank" key={index}>
+          <FontAwesomeIcon
+            icon={social.icon}
+            className="text-3xl text-grayscale-0 hover:scale-110  transition-all duration-75"
+          />
+        </a>
+      ))}
     </div>
   );
 };
